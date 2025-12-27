430b08639b4613ec62d61912585e0c8cd8151e1d
Furkan Hasanusta <furkanhasanusta11@gmail.com>
2025-12-27 10:57:48 +0300

    Initial Commit

---
+++ Models/Sponsors.cs
@@ -0,0 +1,9 @@
+﻿namespace KAR_Robotics.Models
+{
+    public class Sponsors
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string photoLink { get; set; }
+    }
+}
