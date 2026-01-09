a8286554b94c7334d9e458863e704ffd48fd0ddf
MathRixx <furkanhasanusta11@gmail.com>
2026-01-09 09:03:57 +0300

    gitignore updated for CI/CD and sponsors deleted.

--- Models/Sponsors.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KAR_Robotics.Models
-{
-    public class Sponsors
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = null!;
-        public string photoLink { get; set; }
-    }
-}
